233e9af48a34ec7dd44aa9b91bdd21060c586181
danielbz88 <daniel.benzvi@post.idc.ac.il>
2015-11-18 09:14:01 +0200

    start of daniel's feature.. Need to reorganize

---
+++ A16 Ex01 OrGranit 201251238 DanielBenZvi 200461424/A16 Ex01 Or 201251238 Daniel 200461424.sln/523116184522448/EventDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Facebook;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+
+namespace _523116184522448
+{
+    internal class EventDetails
+    {
+        DateTime m_StartingTime;
+        DateTime m_EndingTime;
+        eEventVisibility m_Visibility;
+        string m_Name;
+        Location m_Location;
+
+        EventDetails()
+        {
+
+        }
+    }
+}
---
+++ A16 Ex01 OrGranit 201251238 DanielBenZvi 200461424/A16 Ex01 Or 201251238 Daniel 200461424.sln/523116184522448/eEventVisibility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _523116184522448
+{
+    public enum eEventVisibility
+    {
+        Private = 0,  // default value
+        Public
+    }
+}
